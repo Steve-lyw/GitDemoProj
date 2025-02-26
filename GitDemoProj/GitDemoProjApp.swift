082002efca1f3ec2888ec27be9c60711e336f1ff
Yuwen Lu <yuwen_lu@outlook.com>
2025-02-26 11:18:40 +1100

    My first contents update

--- GitDemoProj/GitDemoProjApp.swift
+++ GitDemoProj/GitDemoProjApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GitDemoProjApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() // This is ContentVIEW
         }
     }
 }
