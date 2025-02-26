0b667c3e20fad89e51881ee9ccefada903631a5d
Yuwen Lu <yuwen_lu@outlook.com>
2025-02-26 11:14:47 +1100

    Initial Commit

---
+++ GitDemoProj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitDemoProj
+//
+//  Created by Yuwen Lu on 26/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitDemoProj/GitDemoProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoProjApp.swift
+//  GitDemoProj
+//
+//  Created by Yuwen Lu on 26/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
